574b9493f8453ae1bd1acf90ede8f6d08cf1210e
loki1129 <roki_1129@yahoo.co.jp>
2012-11-11 20:11:46 +0900

    タイトルに入力を求めるダミーのテキストとスクリプト追加

---
+++ Program/Pictchain/Assets/Resources/Scene/Title/Script/DummyInputTextScript.js
@@ -0,0 +1,38 @@
+#pragma strict
+
+//public value
+var InputText : GUIText;
+var PositiveSpeed = 0.1f;
+var NegativeSpeed = 0.1f;
+
+//private value
+private var Direction : int;
+
+function Awake() {
+	Direction = -1;
+}
+
+function Start () {
+}
+
+function Update () {
+}
+
+function OnGUI() {
+	var alpha : float;
+	alpha = InputText.material.color.a;
+	if (Direction > 0)
+	{
+		Debug.Log("Positive");
+		alpha += PositiveSpeed * Direction;
+		if (alpha >= 1.0f) Direction = -1;
+	}
+	else
+	{
+		Debug.Log("Negative");
+		alpha += NegativeSpeed * Direction;
+		if (alpha <= 0.0f) Direction = 1;
+	}
+	
+	InputText.material.color.a = alpha;
+}
