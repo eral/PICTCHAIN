f6bc34e559d6db43043a12057cad42540cc58b7b
eral.r.l <eral.r.l@gmail.com>
2013-05-18 21:39:00 +0900

    CardInfoテスト

--- Program/Pictchain/Assets/Resources/Scene/TurnCardCompetition/Script/TurnCard.js
+++ Program/Pictchain/Assets/Resources/Scene/TurnCardCompetition/Script/TurnCard.js
@@ -1,12 +1,19 @@
 #pragma strict
 
 private var m_material:Material = null;
+private var m_card_info:CardInfo = null;
 
 function Start () {
-	if (!m_material) {
+	if (!m_card_info) {
+		var obj:GameObject = GameObject.Find("CardInfo");
+		m_card_info = obj.GetComponent(CardInfo);
+		if (!m_card_info) {
+			enabled = false;
+		}
+	}
+	if (enabled && !m_material) {
 		var mr:MeshRenderer = GetComponent(MeshRenderer);
 		m_material = mr.material;
-		
 		if (!m_material) {
 			enabled = false;
 		}
@@ -20,6 +27,19 @@ function OnChangeCard () {
 		color.r = Mathf.Floor(Random.value * color_depth) / (color_depth - 1.0);
 		color.g = Mathf.Floor(Random.value * color_depth) / (color_depth - 1.0);
 		color.b = Mathf.Floor(Random.value * color_depth) / (color_depth - 1.0);
+		Debug.Log("test↓");
+		var card_names = m_card_info.GetAllCardName(0);
+		for (card_name in card_names) {
+			Debug.Log("card_name:" + card_name.card_name + ", category_name:" + card_name.category_name);
+		}
+		Debug.Log("test→");
+		var card_name = m_card_info.GetCardName(0, 'こ');
+		if (null != card_name) {
+			Debug.Log("card_name:" + card_name.card_name + ", category_name:" + card_name.category_name);
+		} else {
+			Debug.Log("null");
+		}
+		Debug.Log("test↑");
 		m_material.color = color;
 	}
 }
