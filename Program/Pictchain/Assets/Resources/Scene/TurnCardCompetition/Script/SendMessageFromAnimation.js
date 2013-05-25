3f901f47e62540cc8bf69f6fba193285bb267287
eral.r.l <eral.r.l@gmail.com>
2013-05-25 19:31:42 +0900

    SendMessageFromAnimation:メッセージ送信ターゲットを送信出来る様に対応

--- Program/Pictchain/Assets/Resources/Scene/TurnCardCompetition/Script/SendMessageFromAnimation.js
+++ Program/Pictchain/Assets/Resources/Scene/TurnCardCompetition/Script/SendMessageFromAnimation.js
@@ -1,13 +1,25 @@
 #pragma strict
 
+public var m_target:GameObject = null;
+
 function SendMessageFromAnimation (message:String) {
-	SendMessage(message, gameObject, UnityEngine.SendMessageOptions.DontRequireReceiver);
+	m_target.SendMessage(message, gameObject, UnityEngine.SendMessageOptions.DontRequireReceiver);
 }
 
 function BroadcastMessageFromAnimation (message:String) {
-	BroadcastMessage(message, gameObject, UnityEngine.SendMessageOptions.DontRequireReceiver);
+	m_target.BroadcastMessage(message, gameObject, UnityEngine.SendMessageOptions.DontRequireReceiver);
 }
 
 function SendMessageUpwardsFromAnimation (message:String) {
-	SendMessageUpwards(message, gameObject, UnityEngine.SendMessageOptions.DontRequireReceiver);
+	m_target.SendMessageUpwards(message, gameObject, UnityEngine.SendMessageOptions.DontRequireReceiver);
 }
+
+function Awake () {
+	if ((null == m_target)) {
+		//Inspectorでの指定が無いなら
+		//自身を対象とする
+		m_target = gameObject;
+	}
+}
+
+
